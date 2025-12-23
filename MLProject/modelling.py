7fa64a2efb5f95ff4e2ad61c2bef9fd9a5dcf661
ainurrach <rachmahnina30@gmail.com>
2025-12-23 16:23:29 +0700

    Fix modelling to MLflow Project entry point

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,64 +1,41 @@
-import os
-import pandas as pd
 import mlflow
 import mlflow.sklearn
+import pandas as pd
+import argparse
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# ===============================
-# Konfigurasi MLflow (WAJIB di CI)
-# ===============================
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("CI_Training_Experiment")
-mlflow.sklearn.autolog()
+def train(data_path, n_estimators, max_depth):
+    mlflow.sklearn.autolog()
 
-def train_model():
-    print("Start training...")
-    print("Current working directory:", os.getcwd())
-    print("Files in directory:", os.listdir())
+    df = pd.read_csv(data_path)
 
-    DATA_PATH = "bank_marketing_preprocessing.csv"
-
-    if not os.path.exists(DATA_PATH):
-        raise FileNotFoundError(
-            f"Dataset '{DATA_PATH}' tidak ditemukan. "
-            f"Pastikan file ada di root repository."
-        )
-
-    df = pd.read_csv(DATA_PATH)
-    df = df.dropna()
-
-    target_col = "y" if "y" in df.columns else "deposit"
-    if target_col not in df.columns:
-        raise ValueError(f"Target column '{target_col}' tidak ditemukan")
-
-    X = df.drop(columns=[target_col])
-    y = df[target_col]
-
-    X = pd.get_dummies(X, drop_first=True)
+    X = df.drop(columns=["deposit"])
+    y = df["deposit"]
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    with mlflow.start_run(run_name="RandomForest_CI"):
-        model = RandomForestClassifier(
-            n_estimators=100,
-            max_depth=10,
-            random_state=42
-        )
-        model.fit(X_train, y_train)
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        random_state=42
+    )
 
-        preds = model.predict(X_test)
-        acc = accuracy_score(y_test, preds)
+    model.fit(X_train, y_train)
 
-        mlflow.log_metric("accuracy", acc)
-
-        print("Training finished successfully")
-        print("Accuracy:", acc)
+    acc = accuracy_score(y_test, model.predict(X_test))
+    mlflow.log_metric("accuracy_manual", acc)
 
 if __name__ == "__main__":
-    train_model()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, required=True)
+    parser.add_argument("--n_estimators", type=int, default=100)
+    parser.add_argument("--max_depth", type=int, default=10)
+    args = parser.parse_args()
+
+    train(**vars(args))
 
